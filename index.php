968c36c9e8ee47022928b2277023f34265b3c649
javi-beltran <72995772+javi-beltran@users.noreply.github.com>
2023-01-20 19:47:19 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Hola</h1>
+    <div>
+    <?php
+    ?>
+    </div>
+    <hr>
+</body>
+</html>
